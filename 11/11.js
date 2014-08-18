de61d53b289e188f29fe5a0c5ef6f161e08afb6b
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2014-08-17 19:01:17 -0700

    Skell code 11

--- 11/11.js
+++ 11/11.js
@@ -1,5 +1,14 @@
 // HTTP File Server
 
-(function() {
+var http = require('http')
 
+(function() {
+    var port = process.argv[2];
+    var input = process.argv[3];
+    var server = http.createServer(function(request, response) {
+        // Request handling logic
+        var src = fs.createReadStream(input);
+
+    });
+    server.listen(port);
 })();
